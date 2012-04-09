e70fe9e2e4a1d97bb77e674d8727df55588eb7d0
Johan Jordaan <johan@whoatemydomain.co.za>
2012-04-09 13:01:04 -0700

    The basics are working now.

--- test/assembler.test.js
+++ test/assembler.test.js
@@ -22,7 +22,7 @@ load_and_execute_testcase_file = function(filename) {
     var code = parts[0].trim();
     var target_byte_code = parts[1].trim().split(' ');
 
-    var byte_code = assembler.generate_byte_code(code);
+    var byte_code = assembler.generate(code).byte_code;
     var byte_code_string = [];
     for(var j=0;j<byte_code.length;j++) 
       byte_code_string.push(byte_code[j].toString(16));
@@ -35,8 +35,8 @@ require('./assembler.test.gen.js');
 
 module.exports = { 
   'test assembler labels ref' : function() { 
-    var code = ':xxx SET PC,xxx';
-    var byte_code = assembler.generate_byte_code(code);
+    var code = 'xxx: SET PC,xxx';
+    var byte_code = assembler.generate(code).byte_code;
     var byte_code_string = [];
     for(var j=0;j<byte_code.length;j++) 
       byte_code_string.push(byte_code[j].toString(16));
@@ -44,14 +44,21 @@ module.exports = {
     assert.ok(check_byte_code(target_byte_code,byte_code),'\n'+code+'\nIs : '+byte_code_string+' Should Be : '+target_byte_code);
   }, 
   'test assembler labels ref lookup' : function() { 
-    var code = ':xxx SET PC,[xxx]';
-    var byte_code = assembler.generate_byte_code(code);
+    var code = 'xxx: SET PC,[xxx]';
+    var byte_code = assembler.generate(code).byte_code;
     var byte_code_string = [];
     for(var j=0;j<byte_code.length;j++) 
       byte_code_string.push(byte_code[j].toString(16));
     var target_byte_code = [0x79c1,0x0000];
     assert.ok(check_byte_code(target_byte_code,byte_code),'\n'+code+'\nIs : '+byte_code_string+' Should Be : '+target_byte_code);
   }, 
+  'test assembler debug' : function() {
+    var code = 'SET  A, [0x10]\nSET PC , 0 ';
+    var debug_info = assembler.generate(code).debug_info;
+    assert.equal(2,debug_info.length);
+    assert.equal('SET A,[0x10]',debug_info[0].code);
+    assert.equal('SET PC,0',debug_info[1].code);
+  },
 
 }
 
