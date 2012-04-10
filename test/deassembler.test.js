00d045c1daf6fcb4cde299c107b5d1157c7eb656
Johan Jordaan <johan@whoatemydomain.co.za>
2012-04-10 06:31:52 -0700

    The deassebler is now in a working condition.

--- test/deassembler.test.js
+++ test/deassembler.test.js
@@ -24,5 +24,30 @@ module.exports = {
     var code = deassembler.deassemble(byte_code);
     assert.equal(code,orig_code);
   }, 
+  'test deassembler' : function() { 
+    var orig_code = 'loop: ADD A,1\nSET PC,loop\n';
+    var target_code = 'loop: ADD A,1\nSET PC,loop\n';
+    var byte_code = assembler.generate(orig_code).byte_code;
+    var labels = {0x0:'loop'};
+    var code = deassembler.deassemble(byte_code,labels);
+    assert.equal(code,target_code);
+  }, 
+  'test deassembler' : function() { 
+    var orig_code = 'SET [data],0x40\ndata: SET [0x40],[0x40]\n';
+    var target_code = 'SET [data],0x40\ndata: SET [0x40],[0x40]\n';
+    var byte_code = assembler.generate(orig_code).byte_code;
+    var labels = {0x3:'data'};
+    var code = deassembler.deassemble(byte_code,labels);
+    assert.equal(code,target_code);
+  }, 
+  'test deassembler' : function() { 
+    var orig_code = 'SET [data],data\ndata: SET [0x40],[0x40]\n';
+    var target_code = 'SET [data],data\ndata: SET [0x40],[0x40]\n';
+    var byte_code = assembler.generate(orig_code).byte_code;
+    var labels = {0x3:'data'};
+    var code = deassembler.deassemble(byte_code,labels);
+    assert.equal(code,target_code);
+  }, 
+
 }
 
